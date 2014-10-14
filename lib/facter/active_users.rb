a53bf0aabc9eeefb7615f2ee59ee77dfb7e3ebb8
Chris Roddy <cmr@mdc2.org>
2014-10-14 18:49:00 -0400

    Revert "update active_users fact" -- apparently this hoses the fuck out of puppet
    This reverts commit 403ff1483315dda19f1546bf0aa978e6904d1263.

--- lib/facter/active_users.rb
+++ lib/facter/active_users.rb
@@ -1,5 +1,3 @@
 Facter.add(:active_users) do
-  setcode do
-    exec('( who; last --since -15min )|head -n -2|cut -d\  -f1|sort|uniq|sed -e \':a;N;$!ba;s/\n/ /g\'')
-  end
+  setcode 'who | cut -d\  -f1|sort|uniq|sed -e \':a;N;$!ba;s/\n/ /g\''
 end
