403ff1483315dda19f1546bf0aa978e6904d1263
Chris Roddy <cmr@mdc2.org>
2014-10-14 17:16:03 -0400

    update active_users fact

--- lib/facter/active_users.rb
+++ lib/facter/active_users.rb
@@ -1,3 +1,5 @@
 Facter.add(:active_users) do
-  setcode 'who | cut -d\  -f1|sort|uniq|sed -e \':a;N;$!ba;s/\n/ /g\''
+  setcode do
+    exec('( who; last --since -15min )|head -n -2|cut -d\  -f1|sort|uniq|sed -e \':a;N;$!ba;s/\n/ /g\'')
+  end
 end
